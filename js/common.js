fc34b8a95e9f700264672c1c3b968497ae20ea16
hahamk <32948160+hahamk@users.noreply.github.com>
2017-12-26 14:55:29 +0900

    event

--- js/common.js
+++ js/common.js
@@ -1,3 +1,5 @@
+var Swiper_4;
+
 // extend Object. inspired by Prototype.
 Object.extend = function(target, source) {
 	for (var property in source)
@@ -168,21 +170,286 @@ var FG_layerPopup;
 	}
 })().load();
 
-
+// 그래프 JS
 $(function() {
-	$('.bxslider').bxSlider({
-		pagerCustom: '.bx-pager',
-		onSlideBefore: function(s, o_idx, n_idx) {
-			if(n_idx == 0 || n_idx == 5 || n_idx == 6 || n_idx == 7) {
-				$('.bx-controls').removeClass('bullet');
-			} else {
-				$('.bx-controls').addClass('bullet');
+    $(".pie").each(function() {
+        var percent = $(this).data("percent"),
+            $left = $(this).find(".left span"),
+            $right = $(this).find(".right span"),
+            deg;
+        
+        if(percent<=50) {
+            // Hide left
+            $left.hide();
+            
+            // Adjust right
+            deg = 180 - (percent/100*360)
+            $right.css({
+                "-webkit-transform": "rotateZ(-"+deg+"deg)"
+            });
+        } else {
+            // Adjust left
+            deg = 180 - ((percent-50)/100*360)
+            $left.css({
+                "-webkit-transform": "rotateZ(-"+deg+"deg)"
+            });
+        }
+    });
+});
+
+// 발자국 JS
+$(function() {
+	$(".vestige-content").hide();
+	$(".vestige-content:first").show();
+	$("ul.boxs li").click(function () {
+		$("ul.boxs li").removeClass("active").css("color", "#363127");
+		//$(this).addClass("active").css({"color":"darkred","font-weight":"bolder"});
+		$(this).addClass("active").css("color", "#ffffff");
+		$(".vestige-content").hide()
+		var activebox = $(this).attr("rel");
+		$("#" + activebox).fadeIn()
+	});
+});
+
+// 안내사항 JS
+	$(".caution-head").click(function(){
+		$(".caution-body").slideToggle('fast',
+			function(){
+				if($(".caution-body").css('display') != 'none'){
+				$(".caution-head").addClass("caution-on");
+				}else{
+					$(".caution-head").removeClass("caution-on");
+				}
+		}
+	);
+});
+$(".caution-head").click(function(){
+	var offset = $(".caution-head").offset();
+  $('html,body').delay(200).animate({scrollTop : offset.top}, 300);
+});
+
+// SWIPER JS
+function swiperjs1(){
+	var swiper_1 = new Swiper('.swiper-container', {
+
+		pagination: '.swiper-pagination',
+		slidesPerView: 2.8, // 보이는 메뉴수
+		paginationClickable: true,
+		spaceBetween: 0,
+		freeMode: true,
+		pagination: false
+	});
+
+	$('.tabs > li > a').on('click', function(e){
+	$(this).parent().addClass('active').siblings().removeClass('active');
+		var tabIdx = $(this).parent().index();
+		swiper_2.slideTo(tabIdx+1, 300);
+		e.preventDefault();
+	});
+
+	var tabLen = $('.tabs > li').length;
+
+	var swiper_2 = new Swiper('.swiper-container-2', {
+		autoHeight: true,
+		slidesPerView: 1,
+		spaceBetween: 0,
+		pagination: false,
+		loop: true,
+		onInit: function(swiper){},
+		onSlideChangeStart: function(swiper){
+			var idx = swiper.activeIndex-1;
+			if( idx < 0 ) { 
+			idx = tabLen - 1;
+			} else if( idx == tabLen ){
+			idx = 0;
 			}
-		},
-		onSlideAfter: function(s, o_idx, n_idx) {
-			$('.bxslider li').removeClass('active');
-			$('.bxslider .slide-'+(n_idx+1)).addClass('active');
+			$('.tabs > li').removeClass('active').eq(idx).addClass('active');
+			if( idx < tabLen ) {
+			swiper_1.slideTo(idx-1, 300);
+			}
 		}
 	});
-	
+}
+
+function swiperjs2(){
+	var swiper_3 = new Swiper('.swiper-container', {
+
+		pagination: '.swiper-pagination',
+		slidesPerView: 2.5, // 보이는 메뉴수
+		paginationClickable: true,
+		spaceBetween: 0,
+		freeMode: true,
+		pagination: false
+	});
+
+	$('.tabs > li > a').on('click', function(e){
+	$(this).parent().addClass('active').siblings().removeClass('active');
+		var tabIdx = $(this).parent().index();
+		Swiper_4.slideTo(tabIdx+1, 300);
+		e.preventDefault();
+	});
+
+	var tabLen = $('.tabs > li').length;
+
+	Swiper_4 = new Swiper('.swiper-container-2', {
+		autoHeight: true,
+		slidesPerView: 1,
+		spaceBetween: 0,
+		pagination: false,
+		loop: true,
+		onInit: function(swiper){},
+		onSlideChangeStart: function(swiper){
+			var idx = swiper.activeIndex-1;
+			if( idx < 0 ) { 
+			idx = tabLen - 1;
+			} else if( idx == tabLen ){
+			idx = 0;
+			}
+			$('.tabs > li').removeClass('active').eq(idx).addClass('active');
+			if( idx < tabLen ) {
+			swiper_3.slideTo(idx-1, 300);
+			}
+		},
+		// css 모션
+		onTransitionEnd: function(swiper){
+			var idx = swiper.activeIndex % 6;
+			if(idx == 1){
+				// alert('첫번째페이지');
+			$(".all-music-full").addClass( "full-up" );
+			$(".all-music-txt p").addClass( "fade" );
+			$(".all-music-txt strong").addClass( "fade" );
+			$(".all-music-txt span").addClass( "fade" );
+			} else if (idx == 5){
+				// alert('다섯번째 페이지');		
+					$(".like-artist5").addClass( "full-left5" );
+					$(".like-artist4").addClass( "full-left4" );
+					$(".like-artist3").addClass( "full-left3" );
+					$(".like-artist2").addClass( "full-left2" );
+					$(".like-artist1").addClass( "full-left1" );
+					$(".like-num1").addClass( "fade" );
+					$(".like-num2").addClass( "fade" );
+					$(".like-num3").addClass( "fade" );
+					$(".like-num4").addClass( "fade" );
+					$(".like-num5").addClass( "fade" );
+			} else {
+					// alert('나머지페이지');
+					$(".all-music-full").removeClass( "full-up" );
+					$(".all-music-txt p").removeClass( "fade" );
+					$(".all-music-txt strong").removeClass( "fade" );
+					$(".all-music-txt span").removeClass( "fade" );
+					$(".like-num1").removeClass( "fade" );
+					$(".like-num2").removeClass( "fade" );
+					$(".like-num3").removeClass( "fade" );
+					$(".like-num4").removeClass( "fade" );
+					$(".like-num5").removeClass( "fade" );
+					$(".like-artist5").removeClass( "full-left5" );
+					$(".like-artist4").removeClass( "full-left4" );
+					$(".like-artist3").removeClass( "full-left3" );
+					$(".like-artist2").removeClass( "full-left2" );
+					$(".like-artist1").removeClass( "full-left1" );
+					}
+		}	
+		// css 모션 end
+	});
+
+	var swiper_5 = new Swiper('.swiper-container-sub', {
+		slidesPerView: 1,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+	});
+}
+
+// 글자자르기
+function textS(){
+
+$('.play-info-toplist strong').each(function(){  
+	var length = 15;		
+	$(this).each(function(){
+		if( $(this).text().length >= length ){
+			$(this).text($(this).text().substr(0,length)+'...');
+		}
+	});
 });
+
+
+$('.play-info strong').each(function(){  
+	var length = 10;		
+	$(this).each(function(){
+		if( $(this).text().length >= length ){
+			$(this).text($(this).text().substr(0,length)+'...');
+		}
+	});
+});
+
+$('.play-info span').each(function(){  
+	var length = 15;		
+	$(this).each(function(){
+		if( $(this).text().length >= length ){
+			$(this).text($(this).text().substr(0,length)+'...');
+		}
+	});
+});
+
+$('.review-txt p').each(function(){  
+	var length = 35;		
+	$(this).each(function(){
+		if( $(this).text().length >= length ){
+			$(this).text($(this).text().substr(0,length)+'...');
+		}
+	});
+});
+
+$('.review-txt p').each(function(){  
+	var length = 31;		
+	$(this).each(function(){
+		if( $(this).text().length >= length ){
+			$(this).text($(this).text().substr(0,length)+'...');
+		}
+	});
+});
+
+$('.review-music-txt p').each(function(){  
+	var length = 13;		
+	$(this).each(function(){
+		if( $(this).text().length >= length ){
+			$(this).text($(this).text().substr(0,length)+'...');
+		}
+	});
+});
+
+$('.review-music-txt span').each(function(){  
+	var length = 15;		
+	$(this).each(function(){
+		if( $(this).text().length >= length ){
+			$(this).text($(this).text().substr(0,length)+'...');
+		}
+	});
+});
+
+$('.play-info-top5-tag').each(function(){  
+	var length = 18;		
+	$(this).each(function(){
+		if( $(this).text().length >= length ){
+			$(this).text($(this).text().substr(0,length)+'');
+		}
+	});
+});
+
+$('.play-info-toplist strong').each(function(){  
+	var length = 15;		
+	$(this).each(function(){
+		if( $(this).text().length >= length ){
+			$(this).text($(this).text().substr(0,length)+'...');
+		}
+	});
+});
+
+}
+
